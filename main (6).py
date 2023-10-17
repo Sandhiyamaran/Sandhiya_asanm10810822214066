1ecc517ecc26624a7591b54b896811b1e152f022
Sandhiyamaran <146066597+Sandhiyamaran@users.noreply.github.com>
2023-10-17 16:16:01 +0530

    Add files via upload

--- main (6).py
+++ main (6).py
@@ -1,33 +1,23 @@
-class bank_account:
-  def __init__(self,account_number,account_holder_name,initial_balance=0.0):
-    self.__account_number=account_number
-    self.__account_holder_name=account_holder_name
-    self.__account_balance=initial_balance
+#Define the base class player
+class player:
+  def play(self):
+    print("the player is playing circket.")
 
-  def deposit(self,amount):
-    if amount>0:
-      self.__account_balance+=amount
-      #self.__account _balance=self.__account_balance+amount
-      print("Deposit ₹{}.New balance: ₹{}".format(amount,self.__account_balance))
-    else:
-       print("invalid deposit amount:")
+#Define the derived class Batsman
+class Batsman(player):
+  def play(self):
+    print("the batsman is batting.")
 
-  def withdraw (self, amount):
-    if amount>0 and amount<= self.__account_balance:
-     self.__account_balance-=amount
-     print("Withdraw ₹{}.New balance:₹{}".format(amount, self.__account_balance))
-    else:
-      print("Invalid withdrawal amount or insufficient balance." )
+#Define thw derived class Bowler
+class Bowler:
+  def play(self):
+    print("the bowler is Bowling.")
 
-  def display_balance(self):
-     print("Account balance for {}(Account #{}):₹{}".format(self.__account_holder_name,self.__account_number,self.__account_balance))
 
-#create an instance of the bank_account class 
-account=bank_account(account_number=123456789,account_holder_name="atm",initial_balance=5000.0)
+#create object of Batsman and Bowler classes
+batsman=Batsman()
+bowler=Bowler()
 
-#test deposit and withdrawal functionality
-account.display_balance()
-account.deposit(500.0)
-account.withdraw(200.0)
-account.withdraw(20000.0)
-account.display_balance()
+#call the play() method for each object
+batsman.play()
+bowler.play()
